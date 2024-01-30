a49bdce022574b822055220dd9b70d95d9bc91ea
Waleed Malik <zenalytiks@gmail.com>
2024-01-30 20:35:18 +0500

    update UI

--- script.js
+++ script.js
@@ -180,14 +180,14 @@ function toggleProjectsGrid(sectionId) {
 )(jQuery);
 
 // PPH Freelancer Profile Link
-(function(d, s) {
-	var useSSL = 'https:' == document.location.protocol;
-	var js, where = d.getElementsByTagName(s)[0],
-	js = d.createElement(s);
+// (function(d, s) {
+// 	var useSSL = 'https:' == document.location.protocol;
+// 	var js, where = d.getElementsByTagName(s)[0],
+// 	js = d.createElement(s);
 	
-	js.src = (useSSL ? 'https:' : 'http:') +  '//www.peopleperhour.com/hire/3594069130/6637713.js?width=300&height=300&orientation=vertical&theme=light&hourlies=782852&rnd='+parseInt(Math.random()*10000, 10);
-	try { where.parentNode.insertBefore(js, where); } catch (e) { if (typeof console !== 'undefined' && console.log && e.stack) { console.log(e.stack); } }
-}(document, 'script'));
+// 	js.src = (useSSL ? 'https:' : 'http:') +  '//www.peopleperhour.com/hire/3594069130/6637713.js?width=300&height=300&orientation=vertical&theme=light&hourlies=782852&rnd='+parseInt(Math.random()*10000, 10);
+// 	try { where.parentNode.insertBefore(js, where); } catch (e) { if (typeof console !== 'undefined' && console.log && e.stack) { console.log(e.stack); } }
+// }(document, 'script'));
 
 
 // function delay() {
